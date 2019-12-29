578e67c11c32c0627354e9b681210449705036d3
BuildTools <unconfigured@null.spigotmc.org>
2019-12-29 10:53:55 +0800

    Adding comment function

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,8 @@ fn main() -> Result<()> {
 
     println!("Rust CLI Blog: v0.0.2 - (c) Max Rumsey 2019");
     println!("Commands:");
-    println!("get/g = Open an entry.");
+		println!("get/g = Open an entry.");
+		println!("comment/m = Make a comment on an entry.");
 		println!("create/c = Create an entry.");
 
     let conn = Connection::open("blog.db")?;
@@ -71,7 +72,7 @@ fn main() -> Result<()> {
 				}
 				conn.execute(
 					"INSERT INTO blog_posts (title, text_content) values (?1, ?2)",
-					&[&title.to_string(), &content.to_string()]
+					&[&title, &content]
 				)?;
 				
 			} else if (command == "get") || (command == "g") {
@@ -81,6 +82,7 @@ fn main() -> Result<()> {
 					"SELECT title, text_content, id from blog_posts
 					 WHERE id = ?"
 				).unwrap();
+				// TODO: Prevent from panicing from non-int.
 				let posts = stmt.query_map(&[id.parse::<i32>().unwrap()], |row|
 					Ok(
 						Post {
@@ -108,6 +110,47 @@ fn main() -> Result<()> {
 					println!("{}", post.text_content);
 					break;
 				}
+			} else if (command == "m") || (command == "comment") {
+				let mut name;
+				let mut id;
+				let mut text;
+				
+				loop {
+					console("Enter Name: ");
+					name = get_input();
+					
+					console("Enter POST ID: ");
+					let orig_post = get_input();
+
+					// TODO: Prevent from panicing from non-int.
+					id = orig_post.parse::<i32>().unwrap();
+					if !does_post_exist(&conn, id) {
+						println!("Post not found! Is the POST ID correct?");
+						continue;
+					}
+					println!("Enter the content of the comment below. When you are finished, type <<FIN>> on a new line:");
+					text = String::new();
+					loop {
+						let input = get_input();
+						if input == "<<FIN>>" {
+							break;
+						} else {
+							text = format!("{}\n{}", text.to_string(), input.to_string())
+						}
+					}
+
+					println!("Is this correct?");
+					console("(Y)es/No: ");
+
+					if (get_input().chars().next().unwrap() == 'Y') || (get_input().chars().next().unwrap() == 'y') {
+						break;
+					}
+				}
+
+				conn.execute(
+					"INSERT INTO blog_comments (author, text_content, parent_post) values (?1, ?2, ?3)",
+					&[&name, &text, &(id.to_string())]
+				)?;
 			}
 		}
 }
@@ -122,4 +165,27 @@ fn console(output: &str) {
 	print!("{}", output);
 
   io::stdout().flush().unwrap();
+}
+fn does_post_exist(conn: &Connection, id: i32) -> bool {
+	let mut stmt = conn.prepare(
+		"SELECT title, text_content, id from blog_posts
+		 WHERE id = ?"
+	).unwrap();
+	let posts = stmt.query_map(&[id], |row|
+		Ok(
+			Post {
+				title: row.get(0).unwrap(),
+				text_content: row.get(1).unwrap(),
+				id: row.get(2).unwrap()
+			}
+		)
+	);
+				 
+	let count = posts.unwrap().count();
+	if count == 0 {
+		println!("No entries found.");
+		return false;
+	} else {
+		return true;
+	}
 }
